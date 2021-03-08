c34910acbac5cbbb5d8e2817db9530c86274b370
Ильгам Ахматдинов <ilgamfa@MacBook-Pro.local>
2021-03-08 14:36:34 +0300

    fix

--- ConsoleApp/ConsoleApp/main.swift
+++
@@ -1,9 +0,0 @@
-//
-//  main.swift
-//  ConsoleApp
-//
-//  Created by Ильгам Ахматдинов on 21.02.2021.
-//
-
-import Foundation
-print("hello world")
--- terminalApp/terminalApp/main.swift
+++
@@ -1,3 +0,0 @@
-protocol CarProtocol {
-    var color: Ui
-}
