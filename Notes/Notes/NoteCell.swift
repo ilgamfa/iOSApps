b5214fde747b1c67b9f8c320e33e50ee2d44f29a
Akhmatdinov Ilgam <ilgamfa@yandex.ru>
2021-04-05 13:13:15 +0300

    add core data to MyNotes

---
+++ Notes/Notes/NoteCell.swift
@@ -0,0 +1,15 @@
+//
+//  NoteCell.swift
+//  Notes
+//
+//  Created by Ильгам Ахматдинов on 05.04.2021.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+}
